e987572ee2d7d82380425f69aefbbad5786170e9
Cássio Vagner dos Santos Diogo <cassiovsd@hotmail.com>
2023-06-21 21:32:33 -0300

    Colocando modal Perfil

--- content/scripts/main.js
+++ content/scripts/main.js
@@ -14,12 +14,12 @@ function init() {
 function checkUsuarioValidado(flValidado) {
     let header = document.getElementById("header");
     
-    if(flValidado === true) {
+    if(flValidado == "true") {
         header.innerHTML = 
         `
         <h1>Convenire</h1>
         <div class="perfil-button-container">
-            <button class="perfil-button">
+            <button id="openModal" class="perfil-button">
                 <div class="perfil-img-container">
                     <img class="perfil-img" src="./content/img/perfil-user.svg" alt="Foto de Perfil">
                 </div>
@@ -34,11 +34,28 @@ function checkUsuarioValidado(flValidado) {
         <div class="header-btns">
             <button><a class="cadastrar" href="register.html">Cadastrar</a></button>
             <button><a class="entrar" href="profile.html">Entrar</a></button>
+            <button id="openModal">Abrir Modal</button>
         </div>
         `;
     }
 }
 
-document.addEventListener('DOMContentLoaded', () => {
-    init()
+document.addEventListener('DOMContentLoaded', async () => {
+    await init().then(function() {
+            var modal = document.getElementById("modal");
+            var openModalButton = document.getElementById("openModal");
+            var closeButton = document.getElementsByClassName("close")[0];
+            openModalButton.onclick = function() {
+            modal.style.display = "block";
+            }
+            closeButton.onclick = function() {
+            modal.style.display = "none";
+            }
+
+            window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    });
 });
--- content/scripts/profile.js
+++ content/scripts/profile.js
@@ -10,6 +10,12 @@ function init() {
 
 function validaCadastro(dados) {
     let flValidado = true;
+    
+    var btnCadastrar = document.getElementById("btn-cadastrar");
+    btnCadastrar.innerHTML = 
+    `
+    <svg class="loader" xmlns="http://www.w3.org/2000/svg" height="1.2rem" viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><style>svg{fill:#ffffff}</style><path d="M304 48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zm0 416a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM48 304a48 48 0 1 0 0-96 48 48 0 1 0 0 96zm464-48a48 48 0 1 0 -96 0 48 48 0 1 0 96 0zM142.9 437A48 48 0 1 0 75 369.1 48 48 0 1 0 142.9 437zm0-294.2A48 48 0 1 0 75 75a48 48 0 1 0 67.9 67.9zM369.1 437A48 48 0 1 0 437 369.1 48 48 0 1 0 369.1 437z"/></svg>
+    `; 
 
     document.querySelectorAll("#form-cadastro input").forEach(d => {
         flValidado = true;
@@ -49,9 +55,28 @@ function validaCadastro(dados) {
             element.insertAdjacentElement("afterend", spanError);
         }
     });
-
+    
     if(flValidado) {
         localStorage.setItem('flValidado', flValidado);
+
+        setTimeout(function() {
+        btnCadastrar.innerHTML = 
+        `
+        <h4>Cadastro concluído!<h4/>
+        `; 
+        btnCadastrar.classList.add("shake-element")
+            //window.location.href = "index.html";
+        }, 2000);
+
+        setTimeout(function() {
+            window.location.href = "index.html";
+        }, 4500);
+
+    } else {
+        btnCadastrar.innerHTML = 
+        `
+        Cadastrar
+        `; 
     }
 
     var minhaFlag = localStorage.getItem('flValidado');
